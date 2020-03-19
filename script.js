f0b57d4b3a7ba3e2bb0a63262c59c12ea0d2174e
Matthew <mrosiles@luc.edu>
2020-03-18 19:38:37 -0500

    Trying to add a script for bringing up options around the home plate

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
     var element = document.getElementById("offcanvas-left");
     element.classList.toggle("hide");
 }
- 
+
 window.toggleRight = function() {
     var element = document.getElementById("offcanvas-right");
     element.classList.toggle("hide");
 }
+
+window.bubbles = function(){
+  var element = document.getElementById("offcanvas-left");
+  element.classList.toggle("hide");
+  var bub =  document.getElementsByClassName('free');
+}
+
+fetch('https://api.edamam.com/api/food-database/parser')
