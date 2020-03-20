85ac822a08f52321b844165fdceeb750da3a007d
Matthew <mrosiles@luc.edu>
2020-03-20 18:25:48 -0500

    Adding functionality for bubble(), also commented out toggleRight()

--- script.js
+++ script.js
@@ -3,15 +3,38 @@
     element.classList.toggle("hide");
 }
 
+/*
 window.toggleRight = function() {
     var element = document.getElementById("offcanvas-right");
     element.classList.toggle("hide");
 }
+*/
 
-window.bubbles = function(){
+function bubbles(){	
   var element = document.getElementById("offcanvas-left");
   element.classList.toggle("hide");
-  var bub =  document.getElementsByClassName('free');
+  var bub =  document.getElementById("bubbles");
+  var bubr = document.getElementById("bubbles-right");
+  if (bub.style.display === "none") {
+    bub.style.display = "block";
+	  if (bubr.style.display === "none") {
+		bubr.style.display = "block";
+	  } else {
+		bubr.style.display = "none";
+	  }  
+  } else {
+    bub.style.display = "none";
+	  if (bubr.style.display === "none") {
+		bubr.style.display = "block";
+	  } else {
+		bubr.style.display = "none";
+	  }  
+  }  
+}
+
+function reset(){
+	var bub =  document.getElementById("bubbles");
+	var bubr = document.getElementById("bubbles-right")
 }
 
 fetch('https://api.edamam.com/api/food-database/parser')
