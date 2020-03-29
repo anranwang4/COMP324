13a52fb97b29d37ecf05a46065dcc74ea27532c1
Matthew <mrosiles@luc.edu>
2020-03-29 11:53:31 -0500

    Moved it from home.html and fixed it up.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.toggleRight = function() {
 }
 */
 
-function bubbles(){	
+function bubbles(){
   var element = document.getElementById("offcanvas-left");
   element.classList.toggle("hide");
   var bub =  document.getElementById("bubbles");
@@ -21,20 +21,39 @@ function bubbles(){
 		bubr.style.display = "block";
 	  } else {
 		bubr.style.display = "none";
-	  }  
+	  }
   } else {
     bub.style.display = "none";
 	  if (bubr.style.display === "none") {
 		bubr.style.display = "block";
 	  } else {
 		bubr.style.display = "none";
-	  }  
-  }  
+	  }
+  }
 }
 
 function reset(){
 	var bub =  document.getElementById("bubbles");
 	var bubr = document.getElementById("bubbles-right")
+}
+
+// Taken from W3Schools!
+function myBtn() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeBtn() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 fetch('https://api.edamam.com/api/food-database/parser')
