389e249bf73e4323187731daee339f6ed1710a00
Jae Kim <jaekimiyo@gmail.com>
2020-04-19 19:55:32 -0500

    omitted JS code as DOM functionality not working yet

--- Jae's UI/script.js
+++ Jae's UI/script.js
@@ -1,6 +1,23 @@
+// window.onscroll = function() {stickyNavBar()};
+// window.onscroll = document.getElementById('navbar');
+// var sticky = navbar.offsetTop(); // "offsetTop returns top position relative to the top of the offsetParent element" (w3schools).
+
+// function stickyNavBar() {
+// 	if (window.pageYOffset >= sticky) {
+// 		navbar.classList.add('sticky')
+// 	}
+// 	else {
+// 		navbar.classList.remove('sticky');
+// 	}
+// }
+
+// Above code from demonstrative purposes from w3schools. Have not implemented yet as we are utilizing CSS selector instead of DOM for the fixed position.
+
+// Toggle button for smaller screens
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active')
 })
+
